4a1fe8fb0f9a730694dd41282354b44f3a81b126
kavitamakariye <kmakariye24@gmail.com>
2019-03-11 20:23:08 +0530

    recursion example

---
+++ Programming-Concepts-Java/src/recursion/Recursion.java
@@ -0,0 +1,18 @@
+package recursion;
+
+public class Recursion {
+
+	static int count = 0;
+
+	static void p() {
+		count++;
+		if (count <= 5) {
+			System.out.println("hello " + count);
+			p();
+		}
+	}
+
+	public static void main(String[] args) {
+		p();
+	}
+}
