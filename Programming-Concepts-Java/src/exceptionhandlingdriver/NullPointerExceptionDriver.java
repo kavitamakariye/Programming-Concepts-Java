32419021e9a68b6ece0eb55318986c813741e0a1
kavitamakariye <kmakariye24@gmail.com>
2019-03-11 20:05:49 +0530

    nullPointer exception example main method

---
+++ Programming-Concepts-Java/src/exceptionhandlingdriver/NullPointerExceptionDriver.java
@@ -0,0 +1,12 @@
+package exceptionhandlingdriver;
+
+import exceptionhandling.NullPointerException;
+
+public class NullPointerExceptionDriver {
+
+	public static void main(String[] args) {
+		
+		NullPointerException noeleex = new NullPointerException();
+		noeleex.printArray();
+	}
+}
