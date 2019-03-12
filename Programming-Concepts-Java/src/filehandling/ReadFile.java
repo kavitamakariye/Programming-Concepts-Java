52cbaf18d7c3b401c69f22c87e8111fe5de76274
kavitamakariye <kmakariye24@gmail.com>
2019-03-12 11:45:33 +0530

    project files

--- Programming-Concepts-Java/src/filehandling/ReadFile.java
+++ Programming-Concepts-Java/src/filehandling/ReadFile.java
@@ -6,16 +6,27 @@ import java.util.Scanner;
 
 public class ReadFile {
 
-public void readDataFromFile(String filePath) throws FileNotFoundException{
-		
+	public void readDataFromFile(String filePath) throws FileNotFoundException {
+
 		File fileObj = new File(filePath);
 		Scanner scanObj = new Scanner(fileObj);
-		
-		while(scanObj.hasNext()){
+
+		while (scanObj.hasNext()) {
 			String line = scanObj.nextLine();
 			System.out.println(line);
 		}
 		scanObj.close();
-		
+
+	}
+
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the full of File you wanna Read.");
+		String filePath = scan.nextLine();
+		scan.close();
+		ReadFile readFile = new ReadFile();
+		readFile.readDataFromFile(filePath);
 	}
 }
